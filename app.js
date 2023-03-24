d26711051c8abd8cfd88bacaee4c49196f058cfc
jyp17 <jypark1024@gmail.com>
2023-03-23 17:35:55 -0700

    Implement tuit controllers

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // app.js creates and configures an HTTP server listening for inco,ing requests
 import express from 'express';
+import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 const app = express();
+app.use(cors()); // import cors first
 app.use(express.json());
 
 HelloController(app);
 UserController(app);
+TuitsController(app);
 
 app.listen(4000);
